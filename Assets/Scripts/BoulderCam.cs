d2b3fb3dc39452ba801fd3931efa02b0a476af6e
Daniel Brior <djbrior@gmail.com>
2023-12-13 00:43:03 -0800

    Add default camera params and score UI

--- Assets/Scripts/BoulderCam.cs
+++ Assets/Scripts/BoulderCam.cs
@@ -6,6 +6,8 @@ public class BoulderCam : MonoBehaviour
 {
     public Transform boulder;
 
+    public float x_offset;
+    public float y_offset;
     public float zoomSpeed = 1f; // Speed of zooming
     public float minOrthoSize = 1f; // Minimum orthographic size
     public float maxOrthoSize = 10f; // Maximum orthographic size
@@ -14,14 +16,17 @@ public class BoulderCam : MonoBehaviour
     void Start()
     {
         cam = GetComponent<Camera>(); // Get the Camera component
+        cam.orthographicSize = 3.0f;
+        x_offset = -0.25f;
+        y_offset = 0.55f;
     }
 
     void Update()
     {
-        transform.position = new Vector3(boulder.position.x, boulder.position.y, transform.position.z);
+        transform.position = new Vector3(boulder.position.x + x_offset, boulder.position.y + y_offset, transform.position.z);
 
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        cam.orthographicSize -= scroll * zoomSpeed;
-        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minOrthoSize, maxOrthoSize);
+        // float scroll = Input.GetAxis("Mouse ScrollWheel");
+        // cam.orthographicSize -= scroll * zoomSpeed;
+        // cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minOrthoSize, maxOrthoSize);
     }
 }
---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Transform boulder;
+    public TextMeshProUGUI tmp;
+
+    private float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0.0f;
+        // tmp = gameObject.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = Mathf.Round(boulder.position.x);
+        tmp.text = "Score: " + score.ToString();
+    }
+}
