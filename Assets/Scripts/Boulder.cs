8839a2fb2e12225a51a0871bd61416468bca38d5
Daniel Brior <djbrior@gmail.com>
2024-06-01 17:47:08 -0700

    Add prestige tech

--- Assets/Scripts/Boulder.cs
+++ Assets/Scripts/Boulder.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class Boulder : MonoBehaviour
 {
     public float mass = 1.0f;
+    private Rigidbody2D rb;
     public float tapDecayMps2;
     public Material glowMaterial;
-    private float currPrestige = 0f;
+    public int currPrestige = 0;
     public float maxGlowThickness = 0.03f;
     public float prestigeSpeed;
     public float glowStartPercent;
     private float glowStartSpeed;
     private float lastBelowSpeedTime;
     public AudioSource prestigeDrum;
+    public AudioSource prestigeMusic;
 
     private float currDrumDelta = 0f;
     private float drumInterval = 1f;
     private float atPrestigeDrumInterval = 0.5f;
     private float drumLastPlayedTime = 0f;
+    private float belowPrestigeSpeedTime = 0f;
+    private bool onCooldown = false;
 
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -32,7 +36,15 @@ public class Boulder : MonoBehaviour
     }
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         glowStartSpeed = glowStartPercent * prestigeSpeed;
+        belowPrestigeSpeedTime = Time.time;
+    }
+
+    IEnumerator Cooldown(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        onCooldown = false;
     }
 
     void Update()
@@ -44,16 +56,25 @@ public class Boulder : MonoBehaviour
             glowMaterial.SetFloat("_Thickness", Mathf.Min(currRatio * maxGlowThickness, maxGlowThickness));
 
             // Drum sound
-            if ((WorldController.Instance.smoothedSpeed >= prestigeSpeed) && ((Time.time - drumLastPlayedTime) >= atPrestigeDrumInterval) && (currDrumDelta >= atPrestigeDrumInterval)) {
+            if (!onCooldown && (WorldController.Instance.smoothedSpeed >= prestigeSpeed) && ((Time.time - drumLastPlayedTime) >= atPrestigeDrumInterval) && (currDrumDelta >= atPrestigeDrumInterval)) {
                 prestigeDrum.Play();
                 drumLastPlayedTime = Time.time;
                 currDrumDelta = 0f;
-            } else if (((Time.time - drumLastPlayedTime) >= drumInterval) && (currDrumDelta >= drumInterval)) {
+                if (Time.time - belowPrestigeSpeedTime > 5f) {
+                    rb.mass *= 10;
+                    prestigeMusic.Play();
+                    currPrestige += 1;
+                    onCooldown = true;
+                    StartCoroutine(Cooldown(10f));
+                }
+            } else if (!onCooldown && ((Time.time - drumLastPlayedTime) >= drumInterval) && (currDrumDelta >= drumInterval)) {
                 prestigeDrum.Play();
                 drumLastPlayedTime = Time.time;
                 currDrumDelta = 0f;
+                belowPrestigeSpeedTime = Time.time;
             }
         } else {
+            belowPrestigeSpeedTime = Time.time;
             glowMaterial.SetFloat("_Thickness", 0f);
         }
     }
