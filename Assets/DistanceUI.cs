b0ec7c44f64dec71bca8e9d4775e364f4fac0a09
Daniel Brior <djbrior@gmail.com>
2024-06-19 13:51:21 -0700

    Change boulder feel at start

--- Assets/DistanceUI.cs
+++ Assets/DistanceUI.cs
@@ -13,6 +13,12 @@ public class DistanceUI : MonoBehaviour
 
     void Update()
     {
-        textUI.text = WorldController.Instance.maxScore.ToString("F0");
+        if (WorldController.Instance.maxScore > 100f) {
+            textUI.text = WorldController.Instance.maxScore.ToString("F0");
+        } else if (WorldController.Instance.maxScore > 10f) {
+            textUI.text = WorldController.Instance.maxScore.ToString("F1");
+        } else {
+            textUI.text = WorldController.Instance.maxScore.ToString("F2");
+        }
     }
 }
--- Assets/ParallaxBackground.cs
+++ Assets/ParallaxBackground.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     public float multiplier;
+    public float teleportX;
+    public Transform partner;
     void Update()
     {
-        float delta = WorldController.Instance.delta;
-        transform.localPosition = new Vector2(
-            transform.localPosition.x - ((delta * multiplier) * 0.01f), 
-            transform.localPosition.y
-        );   
+        if (!WorldController.Instance.frozen) {
+            float delta = WorldController.Instance.delta;
+            transform.localPosition = new Vector2(
+                transform.localPosition.x - ((delta * multiplier) * 0.01f), 
+                transform.localPosition.y
+            );   
+            if (transform.position.x <= teleportX) {
+                if (partner) {
+                    Debug.Log("Tele");
+                    Debug.Log(partner.position);
+                    Debug.Log(transform.position);
+                }
+                transform.position = new Vector2((transform.position.x-teleportX) + (-1*teleportX), transform.position.y);
+                Debug.Log(transform.position);
+            }
+        }
     }
 }
