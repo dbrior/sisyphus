b90e3965cc7fec4db610f6b1c8d34374f718620c
Daniel Brior <djbrior@gmail.com>
2024-06-23 21:13:36 -0700

    Add swipe to jump

---
+++ Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwipeDetection : MonoBehaviour
+{
+    public UnityEvent OnSwipe;
+    private Dictionary<int, Vector2> touchStartPositions = new Dictionary<int, Vector2>();
+    public float swipeThreshold;
+    private Collider2D collider;
+    private Sisyphus sisyphus;
+
+    void Start() {
+        collider = GetComponent<Collider2D>();
+        sisyphus = WorldController.Instance.sisyphus;
+
+        if (OnSwipe == null) OnSwipe = new UnityEvent();
+    }
+
+    void Update()
+    {
+        DetectSwipes();
+    }
+
+    // TODO: Make more robust to different swiping directions
+    void DetectSwipes()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (IsTouchOnObject(touch)) touchStartPositions[touch.fingerId] = touch.position;
+                    break;
+
+                case TouchPhase.Ended:
+                    if (touchStartPositions.TryGetValue(touch.fingerId, out Vector2 startTouchPosition))
+                    {
+                        Vector2 endTouchPosition = touch.position;
+                        if (IsSwipeUp(startTouchPosition, endTouchPosition))
+                        {
+                            OnSwipe.Invoke();
+                        }
+                        touchStartPositions.Remove(touch.fingerId);
+                    }
+                    break;
+            }
+        }
+    }
+
+    bool IsSwipeUp(Vector2 startTouchPosition, Vector2 endTouchPosition)
+    {
+        Vector2 swipeDirection = endTouchPosition - startTouchPosition;
+        Debug.Log("Swipe: " + swipeDirection.ToString());
+        return swipeDirection.y > swipeThreshold && Mathf.Abs(swipeDirection.x) < swipeThreshold / 2;
+    }
+
+    bool IsTouchOnObject(Touch touch)
+    {
+        return collider.OverlapPoint(Camera.main.ScreenToWorldPoint(touch.position));
+    }
+}
